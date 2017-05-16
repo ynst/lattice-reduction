c77adc05df15dfc59f39b9718976019eca96c754
yunustuncbilek <yunus.tuncbilek@yale.edu>
2017-05-16 08:17:32 -0400

    Jia Frechet fixed costs instead of uniform

--- AE_cpp/sources/jia.cpp
+++ AE_cpp/sources/jia.cpp
@@ -14,33 +14,36 @@ double profit(vector<int> decision_vector){
 	for(int i  = 0; i < decision_vector.size(); i++){
 		// random seed must only depend on the index of fixed costs to ensure
 		// same value is output every time profit is calculated
-		srand ( i ); 
-		fixed_costs.push_back(randomFloat(0,1));
+		srand ( i + 10 ); 
+		fixed_costs.push_back(randomFrechet(randomFloat(0,1)));
+		cout << randomFrechet(randomFloat(0,1)) << " " << i << "\n";
 	}
 
 	vector<double> distances(decision_vector.size()*decision_vector.size(), 0);
 
 	for(int i = 0; i < decision_vector.size(); i++){
 		for(int j = 0; j < decision_vector.size(); j++){
-			// random seed must only depend on the index of distance to ensure
-			// same value is output for that distance 
+			// random seed must only depend on the index of distance 
+			// to ensure same value is output for that distance 
 			// every time profit is calculated
 			srand ( i * decision_vector.size() + j );
-			distances[decision_vector.size() * i + j] = randomFloat(0,1);
+			distances[decision_vector.size() * i + j] = 1; //randomFrechet(randomFloat(0,1));
+			// cout << randomFrechet(randomFloat(0,1)) << "\t\t" << randomFloat(0,1) << "\n";
 		}
 	}
 
-	double delta = -1;
+	double delta = -0.01;
 
 	double prof2return = 0;
 
 	for (int i = 0; i < decision_vector.size(); ++i)
 	{
 		if(decision_vector[i]){
-			prof2return+=fixed_costs[i];
+			prof2return+=fixed_costs[i]; //Xi
 			for (int j = 0; j < decision_vector.size(); ++j){
 				if(j!=i && decision_vector[j]){
-					prof2return+=delta/distances[i*decision_vector.size()+ j];
+					prof2return+=delta/distances[i*decision_vector.size()+ j]; 
+					//delta over the distance
 				}
 			}
 		}
