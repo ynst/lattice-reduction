5e389c464bdcb39f359547b3fc804644b9f211a1
yunustuncbilek <yunus.tuncbilek@yale.edu>
2017-05-16 08:13:46 -0400

    Frechet distribution

--- AE_cpp/sources/utils.cpp
+++ AE_cpp/sources/utils.cpp
@@ -12,3 +12,24 @@ float randomFloat(float min, float max)
     float range = max - min;
     return (random*range) + min;
 }
+
+float randomFrechet (float uniform){
+	return (1 * exp(log(-log(uniform))/-3) - 3);
+}
+
+void displayProgressBar(float progress){
+	if (progress < 1.0) {
+	    int barWidth = 70;
+
+	    std::cout << "[";
+	    int pos = barWidth * progress;
+	    for (int i = 0; i < barWidth; ++i) {
+	        if (i < pos) std::cout << "=";
+	        else if (i == pos) std::cout << ">";
+	        else std::cout << " ";
+	    }
+	    std::cout << "] " << int(progress * 100.0) << " %\r";
+	    std::cout.flush();
+	}
+	std::cout << std::endl;
+}
